e5fe7b486d35b39465092b04dce05a9445d447b1
simsh1031 <sshsim1031@gmail.com>
2025-07-31 11:03:55 +0900

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // sout -> System.out.println();
+        System.out.println("Hello World!");
+        // 같은 줄 복사 ctrl + D
+        System.out.println("Hello World!");
+        // 줄 지우기 ctrl + y
+        // 본인 OS에 맞는 IntelliJ 주요 단축키 찾아보기
+        System.out.println("Bye Earth");
+    }
+}
